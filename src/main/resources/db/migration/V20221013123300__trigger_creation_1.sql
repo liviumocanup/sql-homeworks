3c7defd837a9ab78b0b1b38306165f7bde5ef5ef
Liviu Mocanu <liviumocanu20009@gmail.com>
2022-10-13 18:10:02 +0300

    Verify if added column already exists beforehand

--- src/main/resources/db/migration/V20221013123300__trigger_creation_1.sql
+++ src/main/resources/db/migration/V20221013123300__trigger_creation_1.sql
@@ -1,5 +1,17 @@
-ALTER TABLE locations
-    ADD department_amount NUMBER(6);
+DECLARE
+    v_col_exists NUMBER := 0;
+BEGIN
+    SELECT count(*) INTO v_col_exists
+    FROM user_tab_cols
+    WHERE upper(column_name) = 'DEPARTMENT_AMOUNT'
+      AND upper(table_name) = 'LOCATIONS';
+
+    IF (v_col_exists = 0) THEN
+        EXECUTE IMMEDIATE 'ALTER TABLE locations ADD department_amount NUMBER(6)';
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('The column DEPARTMENT_AMOUNT already exists');
+    END IF;
+END;
 
 COMMENT ON COLUMN locations.department_amount IS 'Contains the amount of departments in the location.';
 
